79ddc3a9a45831275df1cb87c84491cfb61e12f2
Find <Findxiaoxun@gmail.com>
2014-12-30 20:41:42 +0800

    v0.3修复一开始提示没有获取root权限的问题

--- src/com/find/coolhosts/MainAct.java
+++ src/com/find/coolhosts/MainAct.java
@@ -1,15 +1,21 @@
 package com.find.coolhosts;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-
-public class MainAct extends Activity {
-
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
-	}
-}
+import android.os.Bundle;  
+import android.app.Activity;  
+import android.widget.Toast;  
+  
+public class MainAct extends Activity {  
+  
+    @Override  
+    public void onCreate(Bundle savedInstanceState) {  
+        super.onCreate(savedInstanceState);  
+        setContentView(R.layout.main);  
+        String apkRoot="chmod 777 "+getPackageCodePath();  
+        System.out.println(getPackageCodePath());
+//        boolean root = RootCheck.RootCommand(apkRoot);  
+        boolean root=RootChecker.hasRoot();
+        Toast.makeText(this, "root: "+root, Toast.LENGTH_SHORT).show();  
+        
+    }  
+   
+}  
