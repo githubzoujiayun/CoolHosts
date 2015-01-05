d676dc6139c4dd8f8bb8effa70ae7d69c8c7032d
Find <Findxiaoxun@gmail.com>
2015-01-05 22:02:50 +0800

    v0.6增加admob

--- src/com/find/coolhosts/CoolHosts.java
+++ src/com/find/coolhosts/CoolHosts.java
@@ -1,11 +1,8 @@
 package com.find.coolhosts;
 
 import java.io.BufferedInputStream;
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
@@ -14,37 +11,108 @@ import java.net.URLConnection;
 
 import org.apache.http.util.ByteArrayBuffer;
 
+import com.google.android.gms.ads.*;
+
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;  
-import android.os.Environment;
 import android.app.Activity;  
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;  
-  
+
 public class CoolHosts extends Activity {  
   
-	String version; 
-	
-	
-	
+	private boolean root;
+	private TextView console;
+	private Button oneKey;
+	private Button help;
+	private Button about;
+	private static final String TAG=CoolHosts.class.getSimpleName();
+	private static final String MY_AD_UNIT_ID="ca-app-pub-8527554614606787/7985243150";
+	private AdView adView;
     @Override  
     public void onCreate(Bundle savedInstanceState) {  
         super.onCreate(savedInstanceState);  
         setContentView(R.layout.main);  
-        String apkRoot="chmod 777 "+getPackageCodePath();  
-        System.out.println(getPackageCodePath());
-//        boolean root = RootCheck.RootCommand(apkRoot);  
-        boolean root=RootChecker.hasRoot();
-        Toast.makeText(this, "root: "+root, Toast.LENGTH_SHORT).show();  
-//        downloadHostsTask.execute(Lib.SOURCE);
+        downloadHostsTask.execute(Lib.SOURCE);
+         
+        about=(Button)findViewById(R.id.about);
+        oneKey=(Button)findViewById(R.id.onekey);
+        help=(Button)findViewById(R.id.help);
+        console=(TextView)findViewById(R.id.console);
+        
+        adView = new AdView(this);
+        adView.setAdUnitId(MY_AD_UNIT_ID);
+        adView.setAdSize(AdSize.BANNER);
+        
+        LinearLayout layout = (LinearLayout)findViewById(R.id.adLayout);
+
+        // 在其中添加adView。
+        layout.addView(adView);
+
+        // 启动一般性请求。
+        AdRequest adRequest = new AdRequest.Builder().build();
+//        AdRequest adRequest = new AdRequest.Builder()
+//        .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)       // 模拟器
+//        .addTestDevice("AC98C820A50B4AD8A2106EDE96FB87D4") // 我的Galaxy Nexus测试手机
+//        .build();
+        // 在adView中加载广告请求。
+        adView.loadAd(adRequest);
 //        String vv = Lib.getlocalversion(getExternalCacheDir().toString());
-//        System.out.println(vv);
 //        if (Lib.setVersion(getExternalCacheDir().toString(),vv)) {
 //			System.out.println("ok");
 //		}
     }  
     public void onResume (){
     	super.onResume();
+    	root=RootChecker.hasRoot();
+    	  adView.resume();
+    	if(!root)
+        	Toast.makeText(this, R.string.unrooted, Toast.LENGTH_SHORT).show();
+    	about.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				AlertDialog.Builder builderAbout = new AlertDialog.Builder(CoolHosts.this);
+				builderAbout.setMessage(R.string.dialog_about);
+				builderAbout.setTitle(R.string.about);
+				builderAbout.setCancelable(true);
+				builderAbout.setPositiveButton("OK", new DialogInterface.OnClickListener(){
+					@Override
+					public void onClick (DialogInterface dialog, int which){dialog.cancel();}});
+				AlertDialog alertAbout = builderAbout.create();
+				alertAbout.show();
+			}
+		});
+    	help.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent();
+				intent.setData(Uri.parse("http://www.findspace.name"));
+				intent.setAction(Intent.ACTION_VIEW);
+				CoolHosts.this.startActivity(intent);
+			}
+		});
+    	
     }
+    /**Update the console textview*/
+    public void appendOnConsole(final int ...id ){
+    	for(int i:id){
+    		console.append(getString(i)+"\n");
+    	}
+    }
+    public void appendOnConsole(final String ...strs){
+    	for(String tempstr:strs)
+    		console.append(tempstr+"\n");
+    }
+    
+    
     
     /**DownLoad hosts file*/
     AsyncTask<String, Void, File> downloadHostsTask = new AsyncTask<String, Void, File>() {
@@ -78,16 +146,32 @@ public class CoolHosts extends Activity {
         @Override
         public void onPostExecute(File f) {
             if (f != null) {
-//                try {
-//                    setVersion(f);
-//                } catch (IOException ioe) {
-//                    ioe.printStackTrace();
-//                }
+                try {
+                	//
+                    Lib.setRemoteVersion(f);
+                    console.setText("");
+                    appendOnConsole(getString(R.string.local_version)+Lib.getlocalversion(Lib.HOSTSPATH));
+                    appendOnConsole(getString(R.string.remote_version)+Lib.getRemoteVersion());
+                } catch (IOException ioe) {
+                    ioe.printStackTrace();
+                }
             }
         }
     };
     
-    
+    @Override
+    public void onPause() {
+      adView.pause();
+      super.onPause();
+    }
+
+
+
+    @Override
+    public void onDestroy() {
+      adView.destroy();
+      super.onDestroy();
+    }
     
 }  
 
--- src/com/find/coolhosts/Lib.java
+++ src/com/find/coolhosts/Lib.java
@@ -7,12 +7,16 @@ import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 
+import android.util.Log;
+
 public final class Lib {
 	static final String SOURCE="http://www.findspace.name/adds/hosts";
+	private static final String TAG=Lib.class.getSimpleName();
 	static final String NOT_EXIST="Don't Exist.";
 	static final String READ_ERROR="Read Error.";
 	static final String TIMEMARK_HEAD="#+UPDATE_TIME";
 	static final String LOG_NAME="CoolHosts";
+	static final String HOSTSPATH="/system/etc/hosts";
 	static String REMOTE_VERSION="";
 	/**Set the local version mark.
 	 * 
@@ -69,6 +73,7 @@ public final class Lib {
 				if(tempstr.contains(Lib.TIMEMARK_HEAD)){
 					versionText=tempstr.substring(Lib.TIMEMARK_HEAD.length()+1);
 					getit=true;
+					Log.d(TAG, "Got local hosts");
 					break;
 				};
 			}
@@ -84,8 +89,21 @@ public final class Lib {
 		}
     	return versionText;
     }
-	public static void setRemoteVersion(String versionText){
-		REMOTE_VERSION=versionText;
+	public static void setRemoteVersion(File f) throws IOException{
+		try {
+			BufferedReader breader=new BufferedReader(new FileReader(f));
+			String line;
+			while((line=breader.readLine())!=null){
+				if(line.startsWith(Lib.TIMEMARK_HEAD)){
+					REMOTE_VERSION=line.substring(Lib.TIMEMARK_HEAD.length()+1);
+					break;
+				}
+			}
+			breader.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} 
+		
 	}
 	public static String getRemoteVersion(){
 		return REMOTE_VERSION;
